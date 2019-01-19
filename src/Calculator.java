a7d016d8d78fe7e9ecf9dfd77e0964b47d08044f
Taalaibekova Zhazgul <jazusya2011@gmail.com>
2019-01-19 16:03:22 -0600

    addition

--- src/Calculator.java
+++ src/Calculator.java
@@ -15,5 +15,8 @@ public static int divide( int num1, int num2){
     public static double multiply (double num1, double num2){
         return num1*num2;
     }
-
+    public static int addition(int n1, int n2){
+       int result= n1+n2;
+       return result;
+    }
 }
