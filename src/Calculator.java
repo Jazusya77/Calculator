5ce757c6bf855ea3f55b07a912f0a83c3e59cbf2
Jazusya77 <44735469+Jazusya77@users.noreply.github.com>
2019-01-19 15:59:05 -0600

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -15,5 +15,8 @@ public static int divide( int num1, int num2){
     public static double multiply (double num1, double num2){
         return num1*num2;
     }
+    public static int add(int num1, int num2){
+        return num1+num2;
+    }
 
 }
