933f901785e9a9d9b8d612c81f0fe2c69bc64182
Taalaibekova Zhazgul <jazusya2011@gmail.com>
2019-01-19 11:41:39 -0600

    git commit -m "added divide method" added divide method

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,6 +7,9 @@ public class Calculator {
         public static double add(double num1, double num2) {
             return num1 + num2;
         }
+public static int divide( int num1, int num2){
+        return num1/num2;
 
+}
 
 }
