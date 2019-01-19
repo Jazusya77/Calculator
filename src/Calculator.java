0c2623e34290073ba549453b3b0b4517a88900a6
Jazusya77 <44735469+Jazusya77@users.noreply.github.com>
2019-01-19 12:03:39 -0600

    Add multiply method.

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,5 +11,8 @@ public static int divide( int num1, int num2){
       int result=num1/num2;
         return result;
 }
+    
+    public static double multiply (double num1, double num2){
+        return num1*num2;
 
 }
